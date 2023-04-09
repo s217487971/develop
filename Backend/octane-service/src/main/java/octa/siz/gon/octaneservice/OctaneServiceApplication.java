1b4d5f480fbf1c90efd5752f09116a710e54ad25
s217487971 <69649013+s217487971@users.noreply.github.com>
2023-04-10 00:15:31 +0200

    initial backend application (#2)
    Co-authored-by: Sizwe Sibanyoni <sizwe@psybergate.co.za>

---
+++ Backend/octane-service/src/main/java/octa/siz/gon/octaneservice/OctaneServiceApplication.java
@@ -0,0 +1,13 @@
+package octa.siz.gon.octaneservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctaneServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctaneServiceApplication.class, args);
+	}
+
+}
