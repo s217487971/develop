8bc549751c31c1b80cd0105115d2e56a98a95417
Sizwe Sibanyoni <sizwe@psybergate.co.za>
2023-04-10 00:12:41 +0200

    initial backend application

---
+++ Backend/octane-service/src/test/java/octa/siz/gon/octaneservice/OctaneServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package octa.siz.gon.octaneservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OctaneServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
